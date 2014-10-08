1b1f2de342b8721bd6152fdf62f1bd8e7bd61df6
mspline <mspline@live.com>
2014-10-07 23:23:56 -0700

    Solution for 1073 ("Square country").

---
+++ dynprog/timus_1073.cpp
@@ -0,0 +1,45 @@
+// Solution for http://acm.timus.ru/problem.aspx?space=1&num=1073 ("Square Country")
+
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    vector<long> squares;
+
+    for (int i = 0; i*i <= n; ++i)
+        squares.push_back(i*i);
+
+    vector<long> cache;
+    cache.reserve(n + 1);
+
+    cache.push_back(0);
+    cache.push_back(1);
+
+    for (int i = 2; i <= n; ++i)
+    {
+        if (find(squares.cbegin(), squares.cend(), i) != squares.end())
+        {
+            cache.push_back(1);
+            continue;
+        }
+
+        long min = numeric_limits<long>::max();
+
+        for (int j = 1; j < squares.size() && squares[j] < i; ++j)
+        {
+            long val = cache[squares[j]] + cache[i-squares[j]];
+            if (val < min) min = val;
+        }
+
+        cache.push_back(min);
+    }
+
+    cout << cache[n];
+}
