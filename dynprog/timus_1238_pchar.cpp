821290ecd56db5b2eb8670f89dcfada205b145ab
unknown <michste@MICHSTE-4.redmond.corp.microsoft.com>
2015-03-22 17:22:14 -0700

    Got rid of std::string entirely.

--- dynprog/timus_1238_pchar.cpp
+++ dynprog/timus_1238_pchar.cpp
@@ -1,49 +1,49 @@
 // http://acm.timus.ru/problem.aspx?space=1&num=1238 ("Folding")
+// No std::string-s are used, only char*-s. The solution is
+// almost an order of magnitude faster than the one with std::string-s:
+// ~0.09s vs ~0.75s
 
 #include <algorithm>
-#include <map>
-#include <string>
 #include <stdio.h>
 #include <string.h>
 
-using namespace std;
-
 char sz[101];
-string shortest[100][100];
-string empty;
+char shortest[100][100][100];
+const char *empty = "";
 
-const string& solve(size_t start, size_t len)
+const char *solve(size_t start, size_t len)
 {
     if (len == 0) return empty;
 
-    string& min_so_far = shortest[start][len];
+    char *min_so_far = shortest[start][len];
 
-    if (!min_so_far.empty()) return min_so_far;
+    if (*min_so_far != 0) return min_so_far;
 
-    min_so_far = string(sz + start, len);
+    strncpy(min_so_far, sz + start, len);
+    min_so_far[len] = 0;
 
     if (len < 3) return min_so_far;
 
     for (size_t pattern_len = len / 2; pattern_len > 0; --pattern_len)
     {
-        for (size_t i = 0; i < min(pattern_len, len - pattern_len * 2 + 1); ++i)
+        for (size_t i = 0; i < std::min(pattern_len, len - pattern_len * 2 + 1); ++i)
         {
             size_t n_contiguous_copies = 1;
 
             for (size_t j = i + pattern_len; j <= len - pattern_len && strncmp(sz + start + i, sz + start + j, pattern_len) == 0; j += pattern_len)
                 ++n_contiguous_copies;
 
-            const string& pattern_solution = solve(start + i, pattern_len);
+            const char *pattern_solution = solve(start + i, pattern_len);
 
             size_t match_len = pattern_len * n_contiguous_copies;
             char match_solution[200];
 
             if (n_contiguous_copies == 1)
-                strcpy(match_solution, pattern_solution.c_str());
+                strcpy(match_solution, pattern_solution);
             else
             {
                 if (pattern_len > 1 || n_contiguous_copies > 3)
-                    sprintf(match_solution, "%d(%s)", n_contiguous_copies, pattern_solution.c_str());
+                    sprintf(match_solution, "%d(%s)", n_contiguous_copies, pattern_solution);
                 else
                 {
                     strncpy(match_solution, sz + start + i, match_len);
@@ -52,11 +52,11 @@ const string& solve(size_t start, size_t len)
             }
 
             char candidate[200];
-            strcpy(candidate, solve(start, i).c_str());
+            strcpy(candidate, solve(start, i));
             strcat(candidate, match_solution);
-            strcat(candidate, solve(start + i + match_len, len - i - match_len).c_str());
+            strcat(candidate, solve(start + i + match_len, len - i - match_len));
 
-            if (strlen(candidate) < min_so_far.size()) min_so_far = candidate;
+            if (strlen(candidate) < strlen(min_so_far)) strcpy(min_so_far, candidate);
         }
     }
 
@@ -66,5 +66,5 @@ const string& solve(size_t start, size_t len)
 int main()
 {
     scanf("%s", sz);
-    printf("%s", solve(0, strlen(sz)).c_str());
+    printf("%s", solve(0, strlen(sz)));
 }
