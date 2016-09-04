90fc240741e433bb00b2beacb5a56b6d6deb961a
mitempo <mstone@ya.ru>
2016-09-04 05:36:44 -0700

    Compiles

--- stack/unparen.cpp
+++ stack/unparen.cpp
@@ -20,12 +20,14 @@ int getprec(char op)
         {'(', 1}
     };
 
-    auto p = prec.find(op);
+    auto p = precs.find(op);
     return p == precs.end() ? 10 : p->second;
 }
 
 struct node
 {
+    node(char c, unique_ptr<node> l = nullptr, unique_ptr<node> r = nullptr) : c(c), left(forward<unique_ptr<node>>(l)), right(forward<unique_ptr<node>>(r)) {}
+
     char c;
     unique_ptr<node> left;
     unique_ptr<node> right;
@@ -38,20 +40,20 @@ unique_ptr<node> ast(const string& expr)
 
     auto popop = [&]()
     {
-        unique_ptr<node> pn2 = treestk.top();
+        unique_ptr<node> pn2 = move(treestk.top());
         treestk.pop();
-        unique_ptr<node> pn1 = treestk.top();
+        unique_ptr<node> pn1 = move(treestk.top());
         treestk.pop();
 
-        treestk.push(make_unique<node>({opstk.top(), pn1, pn2}));
+        treestk.push(make_unique<node>(opstk.top(), move(pn1), move(pn2)));
         opstk.pop();
-    }
+    };
 
     for (const char *p = expr.c_str(); *p != 0; ++p)
     {
         char c = *p;
         if (isalpha(c))
-            treestk.emplace(c);
+            treestk.push(make_unique<node>(c));
         else if (c == '(')
             opstk.push(c);
         else if (c == ')')
@@ -72,23 +74,23 @@ unique_ptr<node> ast(const string& expr)
     while (!opstk.empty())
         popop();
 
-    return treestk.empty() ? nullptr : treestk.top();
+    return treestk.empty() ? nullptr : move(treestk.top());
 }
 
 string unparen(node *ast)
 {
     if (ast == nullptr) return "";
 
-    if (isalpha(ast->c)) return string(c);
+    if (isalpha(ast->c)) return string(string() + ast->c);
 
     return
         (getprec(ast->c) <= getprec(ast->left->c)
-            ? unparen(ast->left)
-            : "(" + unparen(ast->left) + ")") +
-        c +
-        (getprec(ast->) < getprec(ast->right->c) || getprec(ast->c) == getprec(ast->right->c) && (ast->c == '+' && ast->c == '-'))
-            ? unparen(ast->right)
-            : "(" + unparen(ast->right) + ")";
+            ? unparen(ast->left.get())
+            : "(" + unparen(ast->left.get()) + ")") +
+        ast->c +
+        ((getprec(ast->c) < getprec(ast->right->c) || getprec(ast->c) == getprec(ast->right->c) && (ast->c == '+' && ast->c == '-'))
+            ? unparen(ast->right.get())
+            : "(" + unparen(ast->right.get()) + ")");
 }
 
 int main()
