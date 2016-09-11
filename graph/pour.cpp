dbd284cc38b4b5be51b6579ee19175261d5b2c07
mitempo <mstone@ya.ru>
2016-09-10 23:43:56 -0700

    Pour - still time limit exceeded

--- graph/pour.cpp
+++ graph/pour.cpp
@@ -30,27 +30,27 @@ int solve()
 {
     while (!q.empty())
     {
-        const state& s = q.front().first;
+        state s = q.front().first;
+        int d = q.front().second;
+
+        q.pop();
 
         if (find(v.cbegin(), v.cend(), s) != v.cend())
-        {
-            q.pop();
             continue;
-        }
 
         v.push_back(s);
 
-        if (isfinal(s)) return q.front().second;
+        if (isfinal(s)) return d;
 
-        if (s.a != 0) q.push(make_pair(state{0, s.b}, q.front().second + 1));
-        if (s.b != 0) q.push(make_pair(state{s.a, 0}, q.front().second + 1));
-        if (s.a != ha) q.push(make_pair(state{ha, s.b}, q.front().second + 1));
-        if (s.b != hb) q.push(make_pair(state{s.a, hb}, q.front().second + 1));
+        if (s.a != 0) q.push(make_pair(state{0, s.b}, d + 1));
+        if (s.b != 0) q.push(make_pair(state{s.a, 0}, d + 1));
+        if (s.a != ha) q.push(make_pair(state{ha, s.b}, d + 1));
+        if (s.b != hb) q.push(make_pair(state{s.a, hb}, d + 1));
 
         int m = min(s.a, hb - s.b);
-        if (m != 0) q.push(make_pair(state{s.a - m, s.b + m}, q.front().second + 1));
+        if (m != 0) q.push(make_pair(state{s.a - m, s.b + m}, d + 1));
         m = min(s.b, ha - s.a);
-        if (m != 0) q.push(make_pair(state{s.a + m, hb - m}, q.front().second + 1));
+        if (m != 0) q.push(make_pair(state{s.a + m, hb - m}, d + 1));
     }
 
     return -1;
