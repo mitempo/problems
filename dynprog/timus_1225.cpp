494979aac223839aa0b01c2fbd5f608134989fa1
mitempo <mstone@ya.ru>
2017-05-22 21:17:45 -0700

    Simplified timus_1225.cpp

--- dynprog/timus_1225.cpp
+++ dynprog/timus_1225.cpp
@@ -10,17 +10,12 @@ int main()
     int n;
     scanf("%d", &n);
     
-    vector<long long> end_in_r(n + 1);
-    vector<long long> end_in_w(n + 1);
-    
-    end_in_r[1] = 1;
-    end_in_w[1] = 1;
-    
+    vector<long long> d(n + 1);
+
+    d[1] = 2;
+
     for (int i = 2; i <= n; ++i)
-    {
-        end_in_r[i] = end_in_w[i-1] + end_in_r[i-2];
-        end_in_w[i] = end_in_r[i-1] + end_in_w[i-2];
-    }
-    
-    printf("%lld", end_in_r[n] * 2);
+        d[i] = d[i-1] + d[i-2];
+
+    printf("%lld", d[n]);
 }
