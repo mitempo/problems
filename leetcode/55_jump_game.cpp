f91bad5e11b8c25ed6a1c4f11c2a7dc1affe84ed
mitempo <mstone@ya.ru>
2017-06-10 13:58:31 -0700

    A simpler solution for leetcode_jump_game

--- leetcode/55_jump_game.cpp
+++ leetcode/55_jump_game.cpp
@@ -26,3 +26,17 @@ public:
         return true;
     }
 };
+
+// A simpler one
+
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int maxsofar = 0;
+
+        for (int i = 0; i < nums.size() && i <= maxsofar && maxsofar < nums.size() - 1; ++i)
+            maxsofar = max(maxsofar, i + nums[i]);
+            
+        return maxsofar >= nums.size() - 1;
+    }
+};
