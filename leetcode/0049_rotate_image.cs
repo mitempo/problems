7b5a083f33a1519bd764516dc895b42b8b70f8f9
mitempo <mitempo>
2018-03-04 23:34:03 -0800

    New leetcode

---
+++ leetcode/0049_rotate_image.cs
@@ -0,0 +1,19 @@
+// https://leetcode.com/problems/rotate-image/description/
+
+public class Solution {
+    public void Rotate(int[,] matrix) {
+        int n = matrix.GetLength(0);
+        for (int i = 0; i < n / 2; ++i)
+        {
+            for (int j = i; j < n - i - 1; ++j)
+            {
+                var t = matrix[i, j];
+                matrix[i, j] = matrix[n - j - 1, i];
+                matrix[n - j - 1, i] = matrix[n - i - 1, n - j - 1];
+                matrix[n - i - 1, n - j - 1] = matrix[j, n - i - 1];
+                matrix[j, n - i - 1] = t;
+            }
+        }
+        
+    }
+}
